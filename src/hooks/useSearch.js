a6a363c1dec1044e524626f7fd077d38ae450792
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-06 13:22:20 +0530

    missed await keyword

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
 import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const BASE_URL = "https://customsearch.googleapis.com/customsearch/v1";
 
@@ -9,21 +9,22 @@ const VITE_SEARCH_ENGINE_KEY = import.meta.env.VITE_SEARCH_ENGINE_KEY;
 
 const useSearch = (searchTerm) => {
   const [data, setData] = useState(null);
-//   const [isLoading, setIsLoading] = useState(false);
+  //   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-    //   setIsLoading(true);
+      //   setIsLoading(true);
 
-      const response = axios.get(
+      const response = await axios.get(
         `${BASE_URL}?key=${VITE_API_KEY}&cx=${VITE_SEARCH_ENGINE_KEY}&q=${searchTerm}`
       );
       setData(response?.data);
+      // console.log(data)
     };
     fetchData();
     // setIsLoading(false);
   }, [searchTerm]);
-
+  console.log(data);
   return { data };
 };
 
