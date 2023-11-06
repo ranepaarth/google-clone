2803ae26a7173111862bebabbf47fb5fefb2eb49
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-06 13:27:55 +0530

    used the result item compo

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import PaginationButtons from "./PaginationButtons";
-
+import SearchResultsItem from "./SearchResultsItem";
 
 const SearchResults = ({ data }) => {
-  console.log(data);
+  // console.log(data);
   return (
-    <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-[52]">
+    <div className="mx-auto w-full px-8 sm:pl-[5%] md:pl-[14%] lg:pl-[52]">
       <p className="text-neutral-400 text-md mb-5 mt-3">
         About {data.searchInformation.formattedTotalResults} results (
         {data.searchInformation.formattedSearchTime} seconds)
@@ -13,21 +12,10 @@ const SearchResults = ({ data }) => {
       {data.items?.map((data) => {
         return (
           <div key={data.link} className="max-w-xl mb-8">
-            <div className="group">
-              <a href={data.link} className="text-sm text-neutral-300">
-                {data.formattedUrl}
-              </a>
-              <a href={data.link}>
-                <h2 className="truncate text-xl text-blue-300 font-medium group-hover:underline">
-                  {data.title}
-                </h2>
-              </a>
-            </div>
-            <p className="text-neutral-400 line-clamp-2">{data.snippet}</p>
+            <SearchResultsItem data={data} />
           </div>
         );
       })}
-      {/* <PaginationButtons /> */}
     </div>
   );
 };
