dd98df7a270d856b51cfaab05c33ec7e775ed32c
ranepaarth <2019paarth.rane@ves.ac.in>
2023-12-05 12:09:46 +0530

    minor text color change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -121,7 +121,7 @@ const Home = () => {
           <div className="flex  items-center gap-1 text-8xl font-serif dark:text-neutral-100">
             <p className="text-blue-600 dark:text-neutral-100">G</p>
             <p className="text-red-600 dark:text-neutral-100">o</p>
-            <p className="text-yellow-600 dark:text-neutral-100">o</p>
+            <p className="text-yellow-400 dark:text-neutral-100">o</p>
             <p className="text-blue-600 dark:text-neutral-100">g</p>
             <p className="text-green-600 dark:text-neutral-100">l</p>
             <p className="text-red-600 dark:text-neutral-100">e</p>
