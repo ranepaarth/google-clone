c2cec37cc4a7ec285590db9a9a6b4bc190bfc236
ranepaarth <2019paarth.rane@ves.ac.in>
2023-12-06 20:52:51 +0530

    optional chaining for search results

--- src/components/SearchPage/SearchResultSection/SearchResults.jsx
+++ src/components/SearchPage/SearchResultSection/SearchResults.jsx
@@ -6,12 +6,12 @@ const SearchResults = ({ data }) => {
   return (
     <div className="mx-auto w-full px-8 sm:pl-[5%] md:pl-[14%] lg:pl-[52]">
       <p className="text-neutral-400 text-md mb-5 mt-3">
-        About {data.searchInformation.formattedTotalResults} results (
-        {data.searchInformation.formattedSearchTime} seconds)
+        About {data?.searchInformation.formattedTotalResults} results (
+        {data?.searchInformation.formattedSearchTime} seconds)
       </p>
-      {data.items?.map((data) => {
+      {data?.items?.map((data) => {
         return (
-          <div key={data.link} className="max-w-xl mb-8">
+          <div key={data?.link} className="max-w-xl mb-8">
             <SearchResultsItem data={data} />
           </div>
         );
