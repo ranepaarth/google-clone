5571c7630e4d9ec92bd13088074bb7ed3864813c
ranepaarth <2019paarth.rane@ves.ac.in>
2023-12-07 11:41:06 +0530

    removed redundant code

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 
-// import useSearch from "../hooks/useSearch";
-import mocks from "../mocks.js";
+import useSearch from "../hooks/useSearch";
+// import mocks from "../mocks.js";
 import {
   Footer,
   PaginationButtons,
@@ -15,27 +15,11 @@ const SearchPage = () => {
   const { search } = useLocation();
   const [startIndex, setStartIndex] = useState(1);
   const searchTerm = search?.split("?").filter((item) => item !== "")[0];
-  console.log(searchTerm);
-  const data = mocks;
-  // const { data } = useSearch(searchTerm, startIndex);
+  // console.log(searchTerm);
+  // const data = mocks;
+  const { data } = useSearch(searchTerm, startIndex);
   // console.log(data);
-  const [darkMode, setDarkMode] = useState(localStorage.getItem("theme") === 'dark');
-  useEffect(() => {
-    const theme = localStorage.getItem("theme");
-    if (theme === "dark") setDarkMode(true);
-    else if (theme === "light") setDarkMode(false);
-  },[]);
 
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
-  }, [darkMode]);
-  
   return (
     <>
       <div className="border-b border-neutral-300 dark:border-neutral-500 pb-4">
